6418aba6570050b04c3b40f6f298b75f090ff34c
unknown <qpdevil83@gmail.com>
2024-02-24 23:43:47 +0330

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\pages\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [HomeController::class,'index']);
 
 Route::get('/login', function () {
     return view('pages.login');
