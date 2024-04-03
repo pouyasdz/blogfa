c5ea1c331b2a93d5c45ee796eb93f6b18e61db3a
unknown <qpdevil83@gmail.com>
2024-04-03 14:18:38 +0430

    add landing page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,18 +2,18 @@
 
 @section('main')
     <main class="container mx-auto font-vazir">
-        <div class="flex flex-col w-full h-max bg-red-200">
+        <div class="flex flex-col sm:flex-row sm:justify-center w-full h-max items-center px-3">
             <div class="flex flex-col w1/2">
-                <img class="w-full h-5" src="{{asset('assets/images/landing.svg')}}" alt="">
+                <img class="w-full h-500" src="{{asset('assets/images/landing.svg')}}" alt="">
             </div>
             <div class="flex flex-col w1/2" dir="rtl">
-                <h1 class="text-3xl font-black">بلاگفا</h1>
-                <p class="text-lg font-semibold">یک ابزار قدرتمند برای ساخت و مدریت وبلاگ است. بلاگفا به شما</p>
-                <p class="text-lg font-semibold">کمک میکند تا با سرعت و سهولت اطلاعات،خاطرات، مطالب و مقالات</p>
-                <p class="text-lg font-semibold">خود را در اینترنت منتشر کنید</p>
-                <div class="flex">
-                    <button>ثبت وبلاگ جدید</button>
-                    <button>ورود</button>
+                <h1 class="text-5xl font-black text-blue-900">بلاگـــفا</h1>
+                <p class="text-lg font-semibold text-gray-700">یک ابزار قدرتمند برای ساخت و مدریت وبلاگ است. بلاگفا به شما</p>
+                <p class="text-lg font-semibold text-gray-700">کمک میکند تا با سرعت و سهولت اطلاعات،خاطرات، مطالب و مقالات</p>
+                <p class="text-lg font-semibold text-gray-700">خود را در اینترنت منتشر کنید</p>
+                <div class="flex gap-5 mt-5">
+                    <button class="px-5 py-2 bg-blue-500 transition-colors delay-150 hover:bg-blue-600 text-white rounded-lg shadow-lg shadow-blue-400"><i class="ri-add-line"></i> ثبت وبلاگ جدید </button>
+                    <button class="px-5 py-2 border-2 border-blue-200 rounded-lg transition-colors delay-150 hover:bg-blue-600 hover:border-blue-600 hover:text-white">ورود</button>
                 </div>
             </div>
         </div>
