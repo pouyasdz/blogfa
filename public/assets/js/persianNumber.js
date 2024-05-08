225643c6669dd26c4252aad14dc1adce519d4adf
pouya <pouya.cc83@gmail.com>
2024-05-08 13:36:03 +0330

    v2

--- public/assets/js/persianNumber.js
+++
@@ -1,7 +0,0 @@
-const allElement = document.querySelectorAll(".persian-num");
-
-allElement.forEach(element => {
-    const main = element.innerHTML;
-    const newContent = Number(main).toLocaleString("fa-IR");
-    element.innerHTML = newContent
-})
